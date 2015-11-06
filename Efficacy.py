abb81249523908618791a40b4083bce5bfff88f2
Captain <Captain>
2015-11-05 22:26:39 -0500

    Working preliminary version of Efficacy.py. HopfieldPlotter.py well established with lots of options

---
+++ Efficacy.py
@@ -0,0 +1,207 @@
+
+import numpy as np
+from scipy import ndimage
+from matplotlib import pyplot as plt
+import optparse
+from sys import argv
+import os, glob
+from time import time as sysT
+from HopfieldPlotter import plot_acco
+from Plotter import read_header, get_headinfo
+
+def main():
+	"""
+	NAME:
+		Efficacy.py	
+	
+	PURPOSE:
+		Efficacy metrics for a Hopfield and control simulations, as a function of
+			the discrimination factor Delta.
+	
+	EXECUTION:
+		python Efficacy.py Directory PlotType Flags
+	
+	ARGUMENTS:
+		Directory	Containes files to be analysed
+		PlotType	Select which plot is to be output
+	
+	OPTIONS:
+		PlotType
+			2	Ratio of product rates
+			3	Energy cost
+			5	Time delay
+	
+	FLAGS
+		-p --npoints	200		How many points to keep for plotting
+		-s --showfig	False	
+	
+	OUTPUTS:
+		Desired plots saved to data directory in .png format
+	
+	COMMENTS
+	
+	BUGS AND TODO:
+		-- Use higher-order centered derivatives
+	
+	HISTORY:
+		03/11/2015 Started CS
+	"""
+	me = "Efficacy: "
+	t0 = sysT()
+	
+	## CLA check
+	try:
+		1/int(os.path.isdir(argv[1]))
+		int(argv[2])
+	except:
+		print me+" input issue. Abort.\n\n",main.__doc__; exit()
+	
+	## Networks considered
+	network = ["no-loop ","Hopfield "]
+	
+	## Options
+	parser = optparse.OptionParser()	
+	parser.add_option('-p','--npoints',
+		dest="npoints", default=200, type="int")
+	parser.add_option('-s','--show',
+		dest="showfig", default=False, action="store_true")
+	parser.add_option('-v','--verbose',
+		dest="verbose", default=False, action="store_true")
+	opt = parser.parse_args()[0]
+	npoints = opt.npoints
+	showfig = opt.showfig
+	verbose = opt.verbose
+	
+	##-------------------------------------------------------------------------
+	## DATA COLLECTION
+	
+	## Files in directory
+	datafiles = np.sort(glob.glob("Results/*.txt"))
+	numfiles = len(datafiles); assert numfiles%2==0
+	
+	Delta = np.zeros(numfiles/2)
+	if int(argv[2])==2:
+		ncurves = 2
+		Cordi = np.zeros((numfiles/2,ncurves))
+		Hordi = np.zeros((numfiles/2,ncurves))
+	else:
+		pass
+	
+	## Loop over files
+	for i in range(numfiles/2):
+		
+		## Filenames
+		Hfile = datafiles[i]
+		Cfile = datafiles[i+numfiles/2]
+		
+		## Unpack data
+		time, Cdata, Hdata, Ck, Hk = unpack_data(Cfile, Hfile, npoints, verbose)
+		
+		## Extract Delta
+		Delta[i] = Ck[6]/Ck[1]
+		assert Delta[i]==Hk[7]/Hk[4]==Hk[6]/Hk[1]
+		
+		## Values for ordinate
+		if int(argv[2])==2:
+			Cordi[i] = prod_rate(Cdata[[1,2]],ncurves,True)
+			Hordi[i] = prod_rate(Hdata[[1,2]],ncurves,True)
+		else:
+			print me+"functionality not written yet."; exit()
+	
+	## Sort data
+	sortind = np.argsort(Delta)
+	Delta = Delta[sortind]
+	Cordi = Cordi[sortind]
+	Hordi = Hordi[sortind]
+	print Hordi
+	
+	##-------------------------------------------------------------------------
+	## PLOTTING
+	
+	times = np.linspace(0,1,ncurves+2).round(2).astype("string")
+	colors = ["k","b","r","g","y"]
+	
+	ax = plt.figure().add_subplot(111)
+	for i in range(ncurves):
+		ax.plot(Delta,Cordi[:,i], colors[i]+"--",label=network[0]+times[i+1]+"*tmax")
+		ax.plot(Delta,Cordi[:,i], colors[i]+"x")
+		ax.plot(Delta,Hordi[:,i], colors[i]+"-" ,label=network[1]+times[i+1]+"*tmax")
+		ax.plot(Delta,Hordi[:,i], colors[i]+"o")
+	plot_acco(ax, title="Ratio of product rates for "+network[0]+"and Hopfield networks",\
+				xlabel="$\Delta$", ylabel="Incorrect / Correct Product Rates")	
+	ax.set_xlim([Delta.min(),Delta.max()])
+	## Save plot
+	# figfile = plotfile+"_5ProdRatio.png"
+	# plt.savefig(figfile); print me+"Figure saved to",figfile
+	
+	## Wrap up
+	if verbose: print me+"total execution time:",round(sysT()-t0,2),"seconds"
+	if showfig: plt.show()
+	
+	return
+	
+##=============================================================================
+def unpack_data(Cdatafile, Hdatafile, npoints, verbose):
+	"""
+	Read and pre-process data
+	"""
+	me = "Efficacy.unpack_data: "
+	t0_unpack_data = sysT()
+	
+	## Read data from file
+	try:
+		Chead = read_header(Cdatafile)
+		CN0, Cpars = get_headinfo(Chead)
+		Cdata = np.loadtxt(Cdatafile,skiprows=8,unpack=True)
+		Hhead = read_header(Hdatafile)
+		HN0, Hpars = get_headinfo(Hhead)
+		Hdata = np.loadtxt(Hdatafile,skiprows=8,unpack=True)
+	except TypeError:
+		print me+"file issue. Abort.\n\n"+main.__doc__
+		exit()
+	
+	## Ensure parameters match between the two files
+	assert Cdata.shape==Hdata.shape
+	assert all(Cdata[0]==Hdata[0])
+	assert CN0==HN0
+		
+	## Select relevant columns
+	Cdata = Cdata[[0,4,8,9,10,11,12,13,14]]
+	Hdata = Hdata[[0,4,8,9,10,11,12,13,14]]
+	
+	## Prune in time
+	if Cdata.shape[1] > npoints:
+		Cdata = Cdata[:,np.arange(0,Cdata.shape[1],Cdata.shape[1]/npoints)]
+		Hdata = Hdata[:,np.arange(0,Hdata.shape[1],Hdata.shape[1]/npoints)]
+		
+	## Normalise
+	Cdata[1:] /= CN0
+	Hdata[1:] /= HN0
+	
+	## Define time array for clarity
+	time = Cdata[0]
+	
+	if verbose: print me+"data preparation overhead",round(sysT()-t0_unpack_data,2),"seconds."
+	
+	return time, Cdata, Hdata, Cpars[1], Hpars[1]
+
+##=============================================================================
+def prod_rate(prod, ntimes, ratio=False):
+	"""
+	Calculate the ratio of product formation rates (incorrect / correct rate)
+	over ntimes intervals.
+	prod is an array whose last dimension hols the time series data.
+	"""
+	me = "Efficacy.prod_rate_ratio: "
+	## Cut off trailing points
+	prod = prod[:,:-(prod.shape[-1]%ntimes)]
+	## Split into equal chunks of time
+	chunks = np.array_split(prod, ntimes, axis=1)
+	## Centered derivative
+	prodrate = ndimage.convolve1d(chunks,[-1,0,1],axis=-1).mean(axis=-1)
+	if ratio:	return prodrate[1]/prodrate[0]
+	else:		return prodrate
+
+##=============================================================================
+if __name__ =="__main__":
+	main()
--- HopfieldPlotter.py
+++ HopfieldPlotter.py
@@ -1,12 +1,18 @@
 
 import numpy as np
+import scipy as sp
+from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
 from sys import argv
+import optparse
 from time import time as sysT
-import os
+import os, warnings
 from Plotter import read_header, get_headinfo
 
 np.seterr(divide='ignore',invalid='ignore')
+warnings.filterwarnings("ignore",
+	"No labelled objects found. Use label='...' kwarg on individual plots.",
+	UserWarning)
 
 def main():
 	"""
@@ -18,11 +24,11 @@ def main():
 			network to those of a "control" four-state network.
 	
 	EXECUTION:
-		python HopfieldPlotter.py datafile1 datafile2 plot_type
+		python HopfieldPlotter.py datafile1 datafile2 plot_type flags
 	
 	ARGUMENTS:
 		file1	Time series of reactant concentrations for "control" network
-		file2	Time series of reactant concentrations for Hopfiled network
+		file2	Time series of reactant concentrations for Hopfield network
 		plot_type	Select which plot is to be output
 	
 	OPTIONS:
@@ -31,30 +37,52 @@ def main():
 			3	Product kinetics for both networks on one plot
 			5	Ratio of products for both networks
 			7	Ratio of product rates for both networks
+			11	Time delay for the networks
+			13	Energy consumed by the networks
 			n	A combination of the above based on the prime factors of n
+	
+	FLAGS
+		-p --npoints	200		How many points to keep for plotting
+		-s --showfig	False	
 	
 	OUTPUTS:
 		outfile#	Desired plot(s) saved to data directory in .png format
-					If option integer is "exact", also shows plot on-screen
 	
-	BUGS, COMMENTS AND TODO:
+	COMMENTS
+	
+	BUGS AND TODO:
 		-- Choose npoints more cleverly
+		-- bounds_error in interpolation
+		-- energy plot (13) -- not sure what is interesting
 	
 	HISTORY:
 		12/10/2015 Started
 		17/10/2015 Usability and axis labelling improved
+		23/10/2015 Plotting time delays
+		02/10/2015 Energy consumption multiplot
 	"""
 	me = "HopfieldPlotter.py: "
-	# from time import time
 	t0 = sysT()
 	
-	## Read user input data file
+	##-------------------------------------------------------------------------
+	## INPUT AND DATA READ
+	
+	## Read user input: data files and plot_type
 	try:
-		Edatafile = argv[1]
+		Cdatafile = argv[1]
 		Hdatafile = argv[2]
+		argv[3]
 	except IndexError:
-		print main.__doc__
+		print me+"input issue. Abort.\n\n"+main.__doc__
 		exit()
+	## Options
+	parser = optparse.OptionParser()	
+	parser.add_option('-p','--npoints',
+		dest="npoints", default=200, type="int")
+	parser.add_option('-s','--show',
+		dest="showfig", default=False, action="store_true")
+	opt = parser.parse_args()[0]
+	showfig = opt.showfig
 	
 	## Output filename base
 	plotfile = os.path.splitext(Hdatafile)[0]
@@ -64,37 +92,40 @@ def main():
 	
 	## Unpack data from file
 	try:
-		Ehead = read_header(Edatafile)
+		Ehead = read_header(Cdatafile)
 		EN0, Epars = get_headinfo(Ehead)
-		Edata = np.loadtxt(Edatafile,skiprows=8,unpack=True)
+		Cdata = np.loadtxt(Cdatafile,skiprows=8,unpack=True)
 		Hhead = read_header(Hdatafile)
 		HN0, Hpars = get_headinfo(Hhead)
 		Hdata = np.loadtxt(Hdatafile,skiprows=8,unpack=True)
 	except TypeError:
-		print me+"input issue. Abort."
+		print me+"file issue. Abort.\n\n"+main.__doc__
 		exit()
-	
+		
+	##-------------------------------------------------------------------------
+	## DATA PREPARARION
+
+	## Prune for speed and memory
+	if Cdata.shape[1] > opt.npoints:
+		Cdata = Cdata[:,np.arange(0,Cdata.shape[1],Cdata.shape[1]/opt.npoints)]
+		Hdata = Hdata[:,np.arange(0,Hdata.shape[1],Hdata.shape[1]/opt.npoints)]
+		
 	## Normalise
-	Edata[1:] /= EN0
+	Cdata[1:] /= EN0
 	Hdata[1:] /= HN0
 		
 	## Ensure parameters match between the two files
-	assert Edata.shape==Hdata.shape
-	assert all(Edata[0]==Hdata[0])
+	assert Cdata.shape==Hdata.shape
+	assert all(Cdata[0]==Hdata[0])
 	assert EN0==HN0
 	print me+"simulation parameters\n",\
 		network[0],"{k}\t",Epars[1],"\n",\
 		network[1],"{k}\t",Hpars[1]
 	
-	## Prune for speed
-	npoints = 200
-	if Edata.shape[1]>npoints:
-		Edata = Edata[:,np.arange(0,Edata.shape[1],Edata.shape[1]/npoints)]
-		Hdata = Hdata[:,np.arange(0,Hdata.shape[1],Hdata.shape[1]/npoints)]
+	## Define time array for clarity
+	time = Cdata[0]
 	
-	time = Edata[0]
-	
-##=============================================================================
+	##-------------------------------------------------------------------------
 	## PLOTTING
 	
 	colours = ["k","b","g","r","c","m","y"]
@@ -104,52 +135,112 @@ def main():
 	if float(argv[3])%2==0:
 		fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 		for i in range(1,5):
-			ax1.plot(time,Edata[i,:],  colours[i]+"-", label=labels[0]+str(i))
-			ax1.plot(time,Edata[i+4,:],colours[i]+"--",label=labels[1]+str(i))
+			ax1.plot(time,Cdata[i,:],  colours[i]+"-", label=labels[0]+str(i))
+			ax1.plot(time,Cdata[i+4,:],colours[i]+"--",label=labels[1]+str(i))
 		plot_acco(ax1, title=network[0],ylabel="Fraction $N/N_0$")	
 		for i in range(1,5):
 			ax2.plot(time,Hdata[i,:],  colours[i]+"-", label=labels[0]+str(i))
 			ax2.plot(time,Hdata[i+4,:],colours[i]+"--",label=labels[1]+str(i))
 		plot_acco(ax2, title=network[1])		
-		plt.savefig(plotfile+"_FullKinetics.png")
-		print me+"Figure saved to",plotfile+"_FullKinetics.png"
+		## Save plot
+		figfile = plotfile+"_2FullKinetics.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
 	
 	## Plot naive products against Hopfield products
 	if float(argv[3])%3==0:
 		fig = plt.figure(); ax = fig.add_subplot(111)
-		ax.plot(time,Edata[4,:], "k"+"-", label=network[0]+labels[0])
-		ax.plot(time,Edata[8,:], "k"+"--",label=network[0]+labels[1])
+		ax.plot(time,Cdata[4,:], "k"+"-", label=network[0]+labels[0])
+		ax.plot(time,Cdata[8,:], "k"+"--",label=network[0]+labels[1])
 		ax.plot(time,Hdata[4,:], "b"+"-", label=network[1]+labels[0])
 		ax.plot(time,Hdata[8,:], "b"+"--",label=network[1]+labels[1])
-		plot_acco(ax, title="Product production: "+network[0]+"versus Hopfield")		
-		plt.savefig(plotfile+"_ProdKinetics.png")
-		print me+"Figure saved to",plotfile+"_ProdKinetics.png"
+		plot_acco(ax, title="Product production: "+network[0]+"versus Hopfield")	
+		## Save plot
+		figfile = plotfile+"_3ProdKinetics.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
 	
 	## Plot ratio of products for naive and Hopfield product
 	if float(argv[3])%5==0:
 		ax = plt.figure().add_subplot(111)
-		ax.plot(time,Edata[8,:]/Edata[4,:], "k"+"-",label=network[0])
+		ax.plot(time,Cdata[8,:]/Cdata[4,:], "k"+"-",label=network[0])
 		ax.plot(time,Hdata[8,:]/Hdata[4,:], "b"+"-",label=network[1])
 		ylabel = "Incorrect / Correct Product Concentration"
 		plot_acco(ax, title="Ratio of products for "+network[0]+"and Hopfield networks",\
 					ylabel=ylabel)		
-		plt.savefig(plotfile+"_ProdRatio.png")
-		print me+"Figure saved to",plotfile+"_ProdRatio.png"
+		## Save plot
+		figfile = plotfile+"_5ProdRatio.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
 
 	## Plot derivative of naive product against Hopfield product
 	if float(argv[3])%7==0:
 		ax = plt.figure().add_subplot(111)
-		ax.semilogy(time[:-1],np.diff(Edata[8,:])/np.diff(Edata[4,:]),"k"+"-",label=network[0])
-		ax.semilogy(time[:-1],np.diff(Hdata[8,:])/np.diff(Hdata[4,:]),"b"+"-",label=network[1])
+		# ax.plot(time[:-1],np.diff(Cdata[8,:])/np.diff(Cdata[4,:]),"k"+"-",label=network[0])
+		# ax.plot(time[:-1],np.diff(Hdata[8,:])/np.diff(Hdata[4,:]),"b"+"-",label=network[1])
+		envelope_plot(time[:-1],np.diff(Cdata[8,:],n=1)/np.diff(Cdata[4,:],n=1),\
+			ax=ax,winsize=5,line="k-",label=network[0])
+		envelope_plot(time[:-1],np.diff(Hdata[8,:],n=1)/np.diff(Hdata[4,:],n=1),\
+			ax=ax,winsize=5,line="b-",label=network[1])
+		plt.legend(network, loc="upper left")
+		# ax.plot(time,hopfield_prod(Hdata[1],Hpars[1][:-2])/hopfield_prod(Hdata[1],Hpars[1][[0,6,2,3,7,5]])\
+			# ,"b--",label="prediction")
 		ylabel = "Incorrect / Correct Product Formation Rate"
 		plot_acco(ax, title="Rate of product formation for "+network[0]+"and Hopfield networks",\
 					ylabel=ylabel)
-		plt.savefig(plotfile+"_DtProdRatio.png")
-		print me+"Figure saved to",plotfile+"_DtProdRatio.png"
+		## Save plot
+		figfile = plotfile+"_7ProdRateRatio.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
+	
+	## Plot delay in time for different schemes to achieve a concentration of product
+	if float(argv[3])%11==0:
+		ax = plt.figure().add_subplot(111)
+		ax.plot(*time_delay(time,Cdata[4],Hdata[4]),color="b",label=None)
+		ylabel = "Time delay, $\Delta t$"
+		plot_acco(ax, title="Time delay for product formation between "+network[0]+"and Hopfield networks",\
+					xlabel="Product concentration $[P]$",ylabel=ylabel)
+		## Save plot
+		figfile = plotfile+"_11DtvP.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
+	
+	## Plot energy usage: versus time and correct product, scheme difference, and all contributions
+	## Want breakdown of energy steps
+	if float(argv[3])%13==0:
+		nrgC = Cdata[9:12,:]; nrgH = Hdata[9:12,:]
+		sumC = nrgC.sum(axis=0); sumH = nrgH.sum(axis=0)
+		fig,ax = plt.subplots(2,2)
+		## Versus time
+		ax[0,0].plot(time,sumC,"k-",label=network[0])
+		ax[0,0].plot(time,sumH,"b-",label="Hopfield")
+		plot_acco(ax[0,0], title="Total $E$", ylabel="Energy units per particle, $E/N_0$",\
+			legloc="upper left")
+		ax[1,0].plot(time,sumH-sumC)
+		plot_acco(ax[1,0], title="Difference $\Delta E$", ylabel="Energy units per particle, $E/N_0$")
+		## Versus correct product
+		ax[0,1].plot(Cdata[4]/Cdata[4,-1],sumC,"k-",label=network[0])
+		ax[0,1].plot(Hdata[4]/Hdata[4,-1],sumH,"b-",label="Hopfield")
+		plot_acco(ax[0,1], title="Total $E$", xlabel="Correct product (fraction of final)",\
+			ylabel="Energy units per particle, $E/N_0$", legloc="upper left")
+		## All energy consumtion pathways
+		ax[1,1].plot(time,nrgC[0],"r--",label=network[0]+"BC")
+		# ax[1,1].plot(time,nrgC[1],"g--",label=network[0]+"CA")
+		ax[1,1].plot(time,nrgC[2],"b--",label=network[0]+"CD")
+		ax[1,1].plot(time,nrgH[0],"r-", label="Hopfield BC")
+		ax[1,1].plot(time,nrgH[1],"g-", label="Hopfield CA")
+		ax[1,1].plot(time,nrgH[2],"b-", label="Hopfield CD")
+		plot_acco(ax[1,1], title="Individual steps' consumption", ylabel="Energy units per particle, $E/N_0$",\
+			legloc="upper left")
+		plt.tight_layout()
+		fig.set_size_inches(10,10)
+		## Save plots
+		figfile = plotfile+"_13Energy.png"
+		plt.savefig(figfile); print me+"Figure saved to",figfile
+		if showfig: plt.show()
 	
 	## Wrap up
-	print me+"Execution time",round(sysT()-t0,2),"seconds"
-	if any(np.array([2,3,5,7]) == float(argv[3])): plt.show()
+	if not showfig: print me+"Execution time",round(sysT()-t0,2),"seconds"
 	
 	return
 
@@ -158,35 +249,80 @@ def main():
 def plot_acco(ax, **kwargs):
 	"""
 	Plot accoutrements.
-	kwargs: title, subtitle, ylabel, plotfile
+	kwargs: title, subtitle, xlabel, ylabel, plotfile
 	"""
 	me = "HopfieldPlotter.plot_acco: "
 	try: ax.set_title(kwargs["title"])
 	except: pass
 	try: ax.suptitle(kawrgs["subtitle"])
 	except: pass
-	ax.set_xlabel("Time, $t$", fontsize=14)
+	try: ax.set_xlabel(kwargs["xlabel"], fontsize=14)
+	except: ax.set_xlabel("Time, $t$", fontsize=14)
 	try: ax.set_ylabel(kwargs["ylabel"], fontsize=14)
 	except: ax.set_ylabel("Fraction $N/N_0$", fontsize=14)
 	ax.grid(True)
-	ax.legend(loc="center right", fontsize=11)			
+	try: ax.legend(loc=kwargs["legloc"], fontsize=11)
+	except: ax.legend(loc="best", fontsize=11)
 	return
 	
 
 ##=============================================================================
-def hopfield_error():
+def hopfield_prod(S,rates,E0=1):
 	"""
-	Rate of producing incorrect versus correct product.
+	Rate of producing product.
 	Notation and assumptions follow those in Hopfield 1974.
-	-- same concentration of both substrates.
-	-- discrimination in off-rates
-	-- ...
 	"""
+	kp,k,mp,m,l,w = rates
+	return (kp*mp*w*E0*S / ((k+mp)*(l+w)+kp*(mp+l+w)*S))
 
+##=============================================================================
+def time_delay(x,y1,y2):
+	"""
+	"""
+	me = "HopfieldPlotter.time_delay: "	
+	## Interpolate x as a function of y: x=g(y)
+	g1 = sp.interpolate.interp1d(y1,x, bounds_error=False)
+	g2 = interp1d(y2,x, bounds_error=False)
+	## Array of y-ticks
+	yarr = np.linspace(0,round(np.append(y1,y2).max()+0.05,1),200)
+	## Delta x
+	Dx = g2(yarr)-g1(yarr)	
+	return yarr,Dx
+	
+
+##================================================
+def envelope_plot(x, y, **kwargs):
+	"""
+	Plot smoothed mean with grey envelope.
+	"""
+	## Read kwargs
+	try: ax = kwargs["ax"]
+	except KeyError: ax = plt.gca()
+	try: winsize = kwargs["winsize"]
+	except KeyError: winsize = len(x)/10
+	try: fill = kwargs["fill"]
+	except KeyError: fill = "grey"
+	try: line = kwargs["line"]
+	except KeyError: line = "b-"
+	
+	# Coarsely chunk the data, discarding the last window if it's not evenly
+	# divisible.
+	numwin = x.size // winsize
+	ywin = y[:winsize * numwin].reshape(-1, winsize)
+	xwin = x[:winsize * numwin].reshape(-1, winsize)
+	# Find the min, max, and mean within each window 
+	ymin = ywin.min(axis=1); ymax = ywin.max(axis=1)
+	ymean = ywin.mean(axis=1)
+	xmean = xwin.mean(axis=1)
+
+	fill_artist = ax.fill_between(xmean, ymin, ymax, color=fill, 
+		                  edgecolor='none', alpha=0.5)
+	line, = ax.plot(xmean, ymean, line)
+	return fill_artist, line
+
+##================================================
+	
 ##=============================================================================
 ##=============================================================================
 if __name__=="__main__":
 	main()
-	
-	
-	
